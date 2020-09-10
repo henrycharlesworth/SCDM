fad95e2237ec5e3379c2b56643251fa8a6a544b9
Henry Charlesworth <h.charlesworth@warwick.ac.uk>
2020-09-10 10:46:07 +0100

    ready to deploy

--- SCDM/TOPDM/example_experiments_2.sh
+++ SCDM/TOPDM/example_experiments_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python trajectory_generator.py --dir_name "TwoEggCatchUnderArm-v0" --expt_name "traj_1" --env "TwoEggCatchUnderArm-v0" --num_envs 20 --num_samples_per_search 2000 --base_iterations 80
+python trajectory_generator.py --expt_tag "traj_1" --env "TwoEggCatchUnderArm-v0" --num_envs 20 --num_samples_per_it 2000 --num_iterations 80
 
-python trajectory_generator.py --dir_name "PenSpin-v0" --expt_name "traj_1" --env "PenSpin-v0" --num_envs 20 --num_samples_per_search 4000 --base_iterations 80 --traj_len 250
+python trajectory_generator.py --expt_tag "traj_1" --env "PenSpin-v0" --num_envs 20 --num_samples_per_it 4000 --num_iterations 80 --traj_len 250
