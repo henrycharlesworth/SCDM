cf922d9670ab741fbe5cf05c4e3240e7c5a0e57a
Henry Charlesworth <h.charlesworth@warwick.ac.uk>
2020-12-07 15:57:45 +0000

    hmm

--- SCDM/TOPDM/ablation_p1.sh
+++ SCDM/TOPDM/ablation_p1.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-python trajectory_generator.py --expt_tag "traj_1" --env "EggHandOver-v0" --num_envs 20
+python trajectory_generator.py --expt_tag "traj_1" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_2" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_3" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_4" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_5" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_6" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_7" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_8" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_9" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_10" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_11" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_12" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_13" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_14" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_15" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_16" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_17" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_18" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_19" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_20" --env "HandPenMod" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
 
-python trajectory_generator.py --expt_tag "traj_1" --env "BlockHandOver-v0" --num_envs 20
 
-python trajectory_generator.py --expt_tag "traj_1" --env "PenHandOver-v0" --num_envs 20
-
-python trajectory_generator.py --expt_tag "traj_1" --env "EggCatchUnderarm-v0" --num_envs 20
-
-python trajectory_generator.py --expt_tag "traj_1" --env "EggCatchOverarm-v0" --num_envs 20
-
-python trajectory_generator.py --expt_tag "traj_1" --env "TwoEggCatchUnderArm-v0" --num_envs 20 --num_samples_per_it 2000 --num_iterations 80
-
-python trajectory_generator.py --expt_tag "traj_1" --env "PenSpin-v0" --num_envs 20 --num_samples_per_it 4000 --num_iterations 80 --traj_len 250
+python trajectory_generator.py --expt_tag "traj_1" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_2" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_3" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_4" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_5" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_6" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_7" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_8" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_9" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_10" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_11" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_12" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_13" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_14" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_15" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_16" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_17" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_18" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_19" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
+python trajectory_generator.py --expt_tag "traj_20" --env "EggCatchUnderarm-v0" --num_envs 20 --num_iterations 20 --later_noise_frac=1.0
